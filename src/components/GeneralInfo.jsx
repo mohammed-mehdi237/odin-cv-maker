a8e077a3b2f76f89e7892c93e91c8239cf04fbc7
Mohammed Mehdi <mahdimohammed237@gmail.com>
2025-03-06 06:51:35 +0300

    Start adding functionality to inputs in general info

--- src/components/GeneralInfo.jsx
+++ src/components/GeneralInfo.jsx
@@ -1,13 +1,24 @@
+import { useState } from "react";
 import "../styles/styles.css";
 import { Input } from "./Input";
 
 export const GeneralInfo = () => {
+  const [fullName , setFullName] = useState('');
+  const [email , setEmail] = useState('');
+  const [phone , setPhone] = useState('');
+  const [address , setAddress] = useState('');
+
+
   return (
-    <>
-      <Input title={"Full Name"} />
-      <Input title={"Email"} />
-      <Input title={"Phone Number"} />
-      <Input title={"Address"} />
-    </>
+    <form >
+      <Input title={"Full Name"} state={fullName} setState={setFullName}/>
+      <Input title={"Email"} state={email} setState={setEmail}/>
+      <Input title={"Phone Number"} state={phone} setState={setPhone}/>
+      <Input title={"Address"} state={address} setState={setAddress}/>
+      <button type="submit" onClick={(e)=>{
+      e.preventDefault();
+      console.log(fullName , email , phone , address)
+    }}>save</button>
+    </form>
   );
 };
--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,13 +1,18 @@
 
 
 
-export const Input = ({title, placeholder , type='text'}) =>{
+export const Input = ({title, placeholder , type='text',state, setState }) =>{
   
 
   return(
     <div className="label-input">
       <label htmlFor="">{title}</label>
-      <input type={type} placeholder={placeholder ? placeholder : title}/>
+      <input
+      type={type}
+      placeholder={placeholder ? placeholder : title} 
+      value={state}
+      onChange={e=>setState(e.target.value)}
+      />
     </div>
   )
 }
